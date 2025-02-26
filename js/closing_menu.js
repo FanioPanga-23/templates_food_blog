77683b60f31824208ab6da65ebf22e7a9a40f692
Fanio Panga <faniospanga@gmail.com>
2025-02-26 17:58:29 +0100

    almost donw

---
+++ js/closing_menu.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    document.getElementById('menuHid').style.display = 'block';
+}
+
+function closeMenu() {
+    document.getElementById('menuHid').style.display = 'none';
+}
